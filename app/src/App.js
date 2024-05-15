a8380b40ec8d45203afadcb4468117220fc231f9
lai2-24 <lai2-24@itismeucci.com>
2024-05-15 13:15:59 +0200

    progetto finito

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [newGame, setNewGame] = useState(false);
+  const [loadingNumero, setLoadingNumero] = useState(false);
+
+  const [rispostaId, setId] = useState("");
+  const [rispostaNum, setNum] = useState(2);
+  const [rispostaTentativi, setTentativi] = useState(0);
+  const [input, setInput] = useState(0);
+
+  async function iniziaPartita(){
+    setNewGame(true);
+    const response = await fetch("http://localhost:8080/partita", {method: "POST"});
+    const r = await response.json();
+    setId(r.id);
+    setNewGame(false);
+  }
+
+  async function invioNumero(){
+    setLoadingNumero(true);
+    const response = await fetch(`http://localhost:8080/partita/${rispostaId}`, 
+      {
+          method: "PUT",
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({"numero": input})
+      });
+    const r = await response.json();
+    setNum(r.risultato);
+    setTentativi(r.tentativi);
+    setLoadingNumero(false);
+  }
+
+  function gestisciInput(e){
+    setInput(e.target.value);
+  }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Indovina Numero</h1>
+      <div><button onClick={(iniziaPartita)}>Nuova Partita</button></div>
+
+      {rispostaId === "" &&
+        <></>
+      }
+
+      {rispostaId !== "" &&
+
+          <>
+              {newGame &&
+                <>in caricamento...</>
+              }
+
+              {!newGame &&
+                  <>
+                      <div>
+                          <p>ID: {rispostaId}</p>
+                          <p>Tenatativi: {rispostaTentativi}</p>
+                      </div>
+
+                      <div>
+                        <p>Inserisci un numero tra 1 e 100</p>
+                        <input type="number" value={input} onclick={invioNumero} onInput={gestisciInput}></input>
+                        <button onClick={invioNumero}>Invia</button>
+
+                      </div>
+
+                      {loadingNumero &&
+                        <>in caricamento...</>
+                      }
+
+                      {!loadingNumero &&
+                        <>
+
+                            {rispostaNum === 2 &&
+                              <></>
+                            }
+                            {rispostaNum === 0 &&
+                              <p>Risposta Giusta</p>
+                            }
+                            {rispostaNum === 1 &&
+                              <p>Il numero è troppo grande</p>
+                            }
+                            {rispostaNum === -1 &&
+                              <p>Il numero è troppo piccolo</p>
+                            }
+
+                        </>
+                      }
+
+                  </>
+              }
+          </>
+
+      }
+
+      
     </div>
+
   );
+
 }
 
 export default App;
